ff5bb0d60beb84777ae476c2e9aaccc266837e6d
Jakub <jakub.operacz@student.wat.edu.pl>
2019-09-16 19:54:33 +0200

    po88

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,67 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+
+
+import { HttpClient } from 'selenium-webdriver/http';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from 'src/app/members/member-list/member-list.component';
+import { MessagesComponent } from 'src/messages/messages.component';
+import { ListsComponent } from 'src/lists/lists.component';
+import { appRoutes } from './routes';
+import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+
+
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent, 
+      MemberCardComponent,
+      MemberDetailComponent
+   ],
+   imports: [
+      BrowserModule,
+      //AppRoutingModule,
+      HttpClientModule,
+      BsDropdownModule.forRoot(),
+      FormsModule,
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokenGetter,
+            whitelistedDomains:['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }      
+      })
+   ],
+   providers: [
+      ErrorInterceptorProvider,
+      AuthService,
+      AlertifyService,
+      UserService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,24 @@
+import { Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from 'src/app/members/member-list/member-list.component';
+import { MessagesComponent } from 'src/messages/messages.component';
+import { ListsComponent } from 'src/lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'members/:id', component: MemberDetailComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent}
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'},
+];
